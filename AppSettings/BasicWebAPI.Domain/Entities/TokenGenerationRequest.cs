a29dbcdad0eab458bc145e7fc31641fe885be9f4
Joao Oliveira <oliveira.joao@gmail.com>
2024-02-21 19:16:34 +0000

    JWT Bearer token is not being created but it compiles. See Comments

--- AppSettings/BasicWebAPI.Domain/Entities/TokenGenerationRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AppSettings.BasicWebAPI.Domain.Entities {
-    public class TokenGenerationRequest {
-        public string? Email { get; set; }
-        public string? UserId { get; set; }
-        public Dictionary<string, string>? CustomClaims { get; set; }
-    }
-}
